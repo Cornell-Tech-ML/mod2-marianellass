069685b97ad924ff48feb719afb6c6c8111c1f1a
marianellas <se.marianella@gmail.com>
2024-10-23 21:59:10 -0400

    Finish 2.5

--- README.md
+++ README.md
@@ -17,3 +17,28 @@ python sync_previous_module.py previous-module-dir current-module-dir
 The files that will be synced are:
 
         minitorch/operators.py minitorch/module.py minitorch/autodiff.py minitorch/scalar.py minitorch/scalar_functions.py minitorch/module.py project/run_manual.py project/run_scalar.py project/datasets.py
+
+
+# Task 2.5
+
+## Simple Dataset
+<img src="simpleGraph.png" width="50%">
+<img src="simpleLoss.png" width="50%">
+<img src="simpleList.png" width="50%">
+
+
+## Diagonal Dataset
+<img src="diagGraph.png" width="50%">
+<img src="diagLoss.png" width="50%">
+<img src="diagList.png" width="50%">
+
+
+## Split Dataset
+<img src="splitGraph.png" width="50%">
+<img src="splitLoss.png" width="50%">
+<img src="splitList.png" width="50%">
+
+## XOR Dataset
+<img src="xorGraph.png" width="50%">
+<img src="xorLoss.png" width="50%">
+<img src="xorList.png" width="50%">
--- project/run_tensor.py
+++ project/run_tensor.py
@@ -10,8 +10,35 @@ import minitorch
 def RParam(*shape):
     r = 2 * (minitorch.rand(shape) - 0.5)
     return minitorch.Parameter(r)
+class Network(minitorch.Module):
+    def __init__(self, hidden_layers):
+        super().__init__()
+        self.layer1 = Linear(2, hidden_layers)
+        self.layer2 = Linear(hidden_layers, hidden_layers)
+        self.layer3 = Linear(hidden_layers, 1)
 
-# TODO: Implement for Task 2.5.
+    def forward(self, x):
+        middle = self.layer1.forward(x)
+        end = self.layer2.forward(middle).relu()
+        return self.layer3.forward(end).sigmoid()
+
+
+class Linear(minitorch.Module):
+    def __init__(self, in_size, out_size):
+        super().__init__()
+        self.weights = RParam(in_size, out_size)
+        self.bias = RParam(out_size)
+        self.out_size = out_size
+
+    def forward(self, x):
+        batch, in_size = x.shape
+
+        reshaped_weights = self.weights.value.view(1, in_size, self.out_size)
+        reshaped_input = x.view(batch, in_size, 1)
+        weightedSum = (reshaped_weights * reshaped_input).sum(1)
+        output = weightedSum.view(batch, self.out_size) + self.bias.value.view(self.out_size)
+
+        return output
 
 def default_log_fn(epoch, total_loss, correct, losses):
     print("Epoch ", epoch, " loss ", total_loss, "correct", correct)
